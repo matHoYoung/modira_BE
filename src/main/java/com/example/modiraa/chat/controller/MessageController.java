875180bb969dd5bcb2bef0282f884442f4c84d2a
Kimseungsuu <107012988+Kimseungsuu@users.noreply.github.com>
2022-08-12 02:33:31 +0900

    체팅구현 시작

---
+++ src/main/java/com/example/modiraa/chat/controller/MessageController.java
@@ -0,0 +1,10 @@
+package com.example.modiraa.chat.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @MessageMapping("/chat/message")
+
+}

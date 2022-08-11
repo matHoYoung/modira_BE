bd582759dc6862451845a50a3b2fdfcb650c3483
Kimseungsuu <107012988+Kimseungsuu@users.noreply.github.com>
2022-08-12 01:24:02 +0900

    실전프로젝트 시작

---
+++ src/main/java/com/example/modiraa/ModiraaApplication.java
@@ -0,0 +1,13 @@
+package com.example.modiraa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModiraaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModiraaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modiraa/ModiraaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modiraa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModiraaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

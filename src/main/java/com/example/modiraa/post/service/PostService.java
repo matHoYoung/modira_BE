1f47f344fed5ea4420b4fd4adc68a640e6acc69e
hyeonorof <hyeonorof@gmail.com>
2022-08-15 21:08:36 +0900

    PostService 모임 검색 추가

--- src/main/java/com/example/modiraa/post/service/PostService.java
+++ src/main/java/com/example/modiraa/post/service/PostService.java
@@ -1,10 +1,13 @@
 package com.example.modiraa.post.service;
 
 import com.example.modiraa.post.dto.PostRequestDto;
+import com.example.modiraa.post.dto.PostsResponseDto;
 import com.example.modiraa.post.model.Post;
 import com.example.modiraa.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -28,6 +31,37 @@ public class PostService {
                 .age(postRequestDto.getAge())
                 .build();
         postRepository.save(post);
+    }
+
+    // 모임 검색
+    public Page<PostsResponseDto> searchPosts(String title, String address, Pageable pageable, Long lastId) {
+        log.info("title -> {}", title);
+        log.info("address -> {}", address);
+        log.info("pageable -> {}", pageable);
+        log.info("lastId -> {}", lastId);
+
+        Page<Post> posts = postRepository
+                .findAllByIdLessThanAndTitleContainingAndAddressContains(lastId, title, address, pageable);
+
+        log.info("result=> {}", posts);
+        log.info("result=> {}", posts.getContent());
+
+        return postResponseDto(posts);
+
+    }
+
+    private Page<PostsResponseDto> postResponseDto(Page<Post> postSlice) {
+        return postSlice.map(p ->
+                PostsResponseDto.builder()
+                        .postId(p.getId())
+                        .title(p.getTitle())
+                        .category(p.getCategory())
+                        .address(p.getAddress())
+                        .date(p.getDate())
+                        .numberOfPeople(p.getNumberofpeople())
+                        .menu(p.getMenu())
+                        .build()
+        );
     }
 
     // 모임 삭제

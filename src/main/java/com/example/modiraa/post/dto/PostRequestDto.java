b7a2b99321489cd6c581317da37dbc2d1519cd5c
hyeonorof <hyeonorof@gmail.com>
2022-08-15 20:54:40 +0900

    PostRequestDto 추가

---
+++ src/main/java/com/example/modiraa/post/dto/PostRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.modiraa.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PostRequestDto {
+    private String category;
+    private String title;
+    private String contents;
+    private String address;
+    private String date;
+    private int numberOfPeople;
+    private String menu;
+    private String gender;
+    private String age;
+}

a9d99554d3d9841b2ec0d8d14431d74cb44c1b60
hyeonorof <hyeonorof@gmail.com>
2022-08-15 21:06:20 +0900

    PostRepository 추가

---
+++ src/main/java/com/example/modiraa/post/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.example.modiraa.post.repository;
+
+import com.example.modiraa.post.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAllByIdLessThanAndTitleContainingAndAddressContains(Long lastId,
+                                                                       String title,
+                                                                       String address,
+                                                                       Pageable pageable);
+}

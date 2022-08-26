72e99328b78f8bcbaec2541558a9a1ef49ecd2b0
hyeonorof <hyeonorof@gmail.com>
2022-08-26 14:04:12 +0900

    상세페이지에서 시간 추가

--- src/main/java/com/example/modiraa/post/dto/PostDetailResponseDto.java
+++ src/main/java/com/example/modiraa/post/dto/PostDetailResponseDto.java
@@ -17,6 +17,7 @@ public class PostDetailResponseDto {
     private double latitude;
     private double longitude;
     private String date;
+    private String time;
     private int numberOfPeople;
     private String menu;
     private String limitGender;

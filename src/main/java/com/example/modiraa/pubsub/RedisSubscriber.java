5e2f9079ff5c9cb2a07e44a9a86abbe07e7b3edf
hyeonorof <hyeonorof@gmail.com>
2022-09-06 15:18:12 +0900

    메시지 내용 DB에 저장 되도록 수정

--- src/main/java/com/example/modiraa/pubsub/RedisSubscriber.java
+++ src/main/java/com/example/modiraa/pubsub/RedisSubscriber.java
@@ -1,6 +1,7 @@
 package com.example.modiraa.pubsub;
 
 import com.example.modiraa.model.ChatMessage;
+import com.example.modiraa.repository.ChatMessageRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,16 +15,23 @@ public class RedisSubscriber {
 
     private final ObjectMapper objectMapper;
     private final SimpMessageSendingOperations messagingTemplate;
+    private final ChatMessageRepository chatMessageRepository;
 
-    /**
-     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 Redis Subscriber가 해당 메시지를 받아 처리한다.
-     */
+
+    // Redis에서 메시지가 발행(publish)되면 대기하고 있던 Redis Subscriber가 해당 메시지를 받아 처리한다.
     public void sendMessage(String publishMessage) {
         try {
             // ChatMessage 객채로 맵핑
             ChatMessage chatMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
             // 채팅방을 구독한 클라이언트에게 메시지 발송
             messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(), chatMessage);
+            ChatMessage message = new ChatMessage();
+            message.setType(chatMessage.getType());
+            message.setRoomId(chatMessage.getRoomId());
+            message.setSender(chatMessage.getSender());
+            message.setMessage(chatMessage.getMessage());
+            message.setUserCount(chatMessage.getUserCount());
+            chatMessageRepository.save(message);
         } catch (Exception e) {
             log.error("Exception {}", e);
         }

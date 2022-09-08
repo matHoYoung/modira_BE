33528e096c073b4a750acb39723e1e79ca519cb3
hyeonorof <hyeonorof@gmail.com>
2022-09-08 23:13:01 +0900

    ChatMessage와 Member 연관 관계 매핑

--- src/main/java/com/example/modiraa/dto/ChatMessageRequestDto.java
+++ src/main/java/com/example/modiraa/dto/ChatMessageRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.modiraa.dto;
 
 import com.example.modiraa.model.ChatMessage;
+import com.example.modiraa.model.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,7 +10,7 @@ import lombok.Getter;
 public class ChatMessageRequestDto {
     private ChatMessage.MessageType type;
     private String roomId;
-    private String sender;
+    private Member sender;
     private String message;
     private long userCount;
 }
